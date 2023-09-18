c84c7f7e3380586e00abb884ae4f7616508f6d21
PrapatsornT <Tanggoma@Tans-MBP.local>
2023-09-18 19:07:01 -0400

    Fixed the path issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import './index.css'
 import '../src/style/index.css'
 import App from './App.jsx'
 import AuthProvider from './context/AuthProvider'
 import { CartProvider } from './context/CartContext';
-import { SearchProvider } from './context/searchContext'
-// import 'bootstrap/dist/css/bootstrap.min.css';
+// import { SearchProvider } from './context/searchContext'
+import { SearchProvider } from './context/SearchContext';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
